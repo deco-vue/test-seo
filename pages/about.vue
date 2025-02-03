6b27233bb178286c4b7794b21c78c32dd59b24ee
deco <deco.vue@gmail.com>
2025-02-03 09:45:33 +0500

    update

---
+++ pages/about.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: 'About Page',
+  ogTitle: 'About Page',
+  description: 'About page',
+  ogDescription: 'About page',
+  ogImage: 'https://i3.wp.com/gb7rostov.ru/wp-content/uploads/WhatsApp-Image-2021-03-17-at-11.13.52-1018x1024.jpeg',
+  twitterCard: 'summary_large_image',
+})
+</script>
+
+<template>
+  <div>
+    <h1>About Page</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate quis dolore minima labore quia excepturi
+      voluptatibus recusandae eveniet non, neque repudiandae, consectetur maxime distinctio veritatis sit aliquam
+      placeat magni nisi laudantium adipisci tempore totam veniam voluptates! Dolores maxime atque molestiae neque.
+      Sequi rem veritatis incidunt nobis laborum repellat voluptates libero quas eos pariatur accusamus fugit
+      consequuntur rerum odit, temporibus eum maxime suscipit dignissimos! Quidem, aperiam ipsa a praesentium quas
+      tenetur, cupiditate, non culpa nisi ad voluptates exercitationem fugiat ex asperiores est repellat ut accusamus
+      deleniti consectetur quaerat laboriosam facere quod corrupti. Sit, ducimus quisquam, ipsum natus optio quis culpa
+      voluptatum explicabo hic excepturi fuga cupiditate pariatur laudantium, amet aperiam assumenda eaque earum nihil.
+      Fugit assumenda mollitia inventore blanditiis voluptates laboriosam voluptatibus illo. Totam veniam molestias a ea
+      minus dolore corrupti velit debitis quo vero, natus sunt quas fuga rem autem quaerat, praesentium animi sequi
+      consequuntur laudantium adipisci! Nulla voluptate iure ullam ad nihil quia necessitatibus, reiciendis commodi.
+      Autem molestiae ipsum quos ipsa iure quidem consectetur obcaecati sequi ex fugit dolores itaque, aspernatur
+      suscipit qui, aut maxime blanditiis reiciendis repellat ab iusto, ad temporibus reprehenderit maiores velit? Eaque
+      animi illo fuga eius aut sed, fugiat hic impedit consequatur quod qui, aliquam, iure distinctio sequi laudantium
+      recusandae cupiditate. Ut earum quod laborum, dolorem voluptates officiis velit suscipit, voluptatem facere nam
+      fugit officia numquam aspernatur iusto, neque in provident iste asperiores. Aut quaerat ipsa vitae minima eaque a
+      quidem nam dolor unde quia voluptatum recusandae praesentium quam laborum quasi velit, consequatur accusantium
+      optio corporis tempora rerum modi atque voluptatibus reprehenderit. Laudantium similique iure distinctio, beatae
+      quas veritatis saepe fuga obcaecati. Minima laborum velit hic accusantium labore earum optio, est numquam vitae
+      maiores perspiciatis corrupti nam ullam quasi natus distinctio porro alias, libero non repellat dolorem explicabo,
+      quisquam sequi ducimus. Maiores dolorum amet eaque quasi accusamus dolorem aspernatur autem consectetur
+      repellendus, voluptatum suscipit voluptas deserunt labore eos magnam ullam iusto eum provident saepe repellat.
+      Perferendis corporis porro, labore numquam nisi a et animi deserunt cupiditate quam, vero beatae ut voluptates
+      itaque cumque odit excepturi pariatur aliquid. Nam omnis perspiciatis incidunt magni excepturi ut, mollitia ipsum
+      et eligendi assumenda repellendus dolores nihil, est dignissimos itaque vero placeat obcaecati repudiandae? Sit
+      dolorem, accusantium sint, tempore iure ducimus molestiae assumenda recusandae soluta maiores provident nobis
+      necessitatibus adipisci. In ullam obcaecati officiis optio inventore. Tempora, nemo voluptates pariatur corporis
+      praesentium id fugiat neque corrupti minima repellat explicabo quos.</p>
+  </div>
+</template>
